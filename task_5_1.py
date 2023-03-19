81bc9c2b99116c4b73ec347e30020287f0e6c932
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-03-19 20:59:58 +0200

    Rename sum on suma

--- task_5_1.py
+++ task_5_1.py
@@ -8,13 +8,12 @@ import math
 
 eps = 1e-4  # точність
 n = 1  # номер члена ряду
-a = math.factorial(n) / (3 * n**n)  # перший член ряду
-sum = a  # початкова сума
+a = math.factorial(n) / (3 * n ** n)  # перший член ряду
+suma = a  # початкова сума
 
 while abs(a) >= eps:
     n += 1
-    a = math.factorial(n) / (3 * n**n)
-    sum += a
+    a = math.factorial(n) / (3 * n ** n)
+    suma += a
 
-print("Сума ряду з точністю eps=10^-4 дорівнює:", sum)
-
+print("Сума ряду з точністю eps=10^-4 дорівнює:", suma)
