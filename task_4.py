d1fd13d3fd2673a6cda2dbd37bf012591dbb4fa7
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-03-19 20:19:49 +0200

    Done task 4

---
+++ task_4.py
@@ -0,0 +1,24 @@
+"""Introduction to programming’: Task 2,
+   Kyshynska Taisiia"""
+
+print("""Introduction to programming’: Task 2
+         Kyshynska Taisiia""")
+
+my_str = input('Input six-digit number: ')
+sum_first = 0
+sum_last = 0
+
+# Перевіряємо, чи введено шестизначне число
+if len(my_str) == 6:
+    # Обчислюємо суму перших та останніх трьох цифр
+    for i in range(3):
+        sum_first += int(my_str[i])
+        sum_last += int(my_str[i + 3])
+
+    # Порівнюємо суми перших та останніх трьох цифр
+    if sum_first == sum_last:
+        print("This is a lucky number!")
+    else:
+        print("This is not a lucky number.")
+else:
+    print("Input six-digit number!")
