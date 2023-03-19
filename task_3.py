c888bf660933997bf4d2676e13c9575e11abd311
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-03-19 18:21:19 +0200

    Done task 3

--- task_3.py
+++ task_3.py
@@ -1,1 +1,15 @@
-print('hi')
+"""Introduction to programming’: Task 2,
+   Kyshynska Taisiia"""
+import math
+
+print("""Introduction to programming’: Task 2
+         Kyshynska Taisiia""")
+
+x = float(input('x = '))
+z = float(input('z = '))
+y = (x + math.exp(z - 1)) / (1 - (x**2) * abs(x - z))
+
+if (x**2) * abs(x - z) == 1:
+    print('Pначення змінних виходять за область визначення функції')
+else:
+    print(y)
