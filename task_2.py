6d77716a9c9f6c950788aebcdb60de72ffd5c6b0
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-03-20 08:16:45 +0200

    Correct task 2

--- task_2.py
+++ task_2.py
@@ -1,12 +1,18 @@
 """Introduction to programming’: Task 2,
    Kyshynska Taisiia"""
 
-print("""Introduction to programming’: Task 2
-         Kyshynska Taisiia""")
+print('Introduction to programming’: Task 2')
+print('Kyshynska Taisiia')
 
-x = int(input('x = '))
-y = int(input('y = '))
-z = int(input('z = '))
+TEMPLATE = "Input {} = "
+while True:
+    try:
+        x = int(input(TEMPLATE.format('x')))
+        y = int(input(TEMPLATE.format('y')))
+        z = int(input(TEMPLATE.format('z')))
+        break
+    except ValueError:
+        print("Error: Input numbers")
 
 equal = ((15 - x / y) / (33 ** y + 19.3) + z)
 print(equal)
