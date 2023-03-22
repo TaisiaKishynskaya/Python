967a4206d9ee79a13ba3d5d62870f20849d1a938
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-03-22 11:49:11 +0200

    Modify task 2

--- task_2.py
+++ task_2.py
@@ -4,7 +4,7 @@
 print('Introduction to programming’: Task 2')
 print('Kyshynska Taisiia')
 
-TEMPLATE = "Input {} = "
+TEMPLATE = 'Input {} = '
 while True:
     try:
         x = int(input(TEMPLATE.format('x')))
@@ -12,7 +12,34 @@ while True:
         z = int(input(TEMPLATE.format('z')))
         break
     except ValueError:
-        print("Error: Input numbers")
+        print('Error: Input numbers')
 
-equal = ((15 - x / y) / (33 ** y + 19.3) + z)
-print(equal)
+while True:
+    equal = ((15 - x / y) / (33 ** y + 19.3) + z)
+    print(equal)
+    change_input = input('Do you want to change x, y, z or exit? Input "x", "y", "z" or "exit": ')
+    if change_input.lower() == 'exit':
+        break
+    elif change_input.lower() == 'x':
+        while True:
+            try:
+                x = int(input(TEMPLATE.format('x')))
+                break
+            except ValueError:
+                print('Error: Input a number for x')
+    elif change_input.lower() == 'y':
+        while True:
+            try:
+                y = int(input(TEMPLATE.format('y')))
+                break
+            except ValueError:
+                print('Error: Input a number for y')
+    elif change_input.lower() == 'z':
+        while True:
+            try:
+                z = int(input(TEMPLATE.format('z')))
+                break
+            except ValueError:
+                print('Error: Input a number for z')
+    else:
+        print('Invalid input')
