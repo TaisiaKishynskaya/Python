5d4601bf843f08bfe114ea02481f741af3fe968e
Taisia <113723880+TaisiaKishynskaya@users.noreply.github.com>
2023-03-22 12:28:26 +0200

    Update task_3.py
    Co-authored-by: vloooo <43804299+vloooo@users.noreply.github.com>

--- task_3.py
+++ task_3.py
@@ -10,7 +10,7 @@ x = float(input(TEMPLATE.format('x')))
 z = float(input(TEMPLATE.format('z')))
 
 if isinstance(x, (int, float)) and isinstance(z, (int, float)): # для проверки того, что переменные x и z являются числами,
-    y = (x + m.exp(z - 1)) / (1 - (x ** 2) * abs(x - z)) #а затем продолжаем выполнение кода, если это так.
+    y = (x + m.exp(z - 1)) / (1 - (x ** 2) * abs(x - z)) # а затем продолжаем выполнение кода, если это так.
 
     if (x**2) * abs(x - z) == 1:
         print('The values of the variables go beyond the scope of the function')
