50a4451e8258aaa2beae8c31b12c51f16263bf3e
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-03-28 00:31:04 +0300

    Done task 7 (without unit-test)

--- main.py
+++ main.py
@@ -2,44 +2,44 @@ from task_7 import Instructor
 
 
 def main():
-    instructor = Instructor("Doe", "John", {})
-    print(instructor)  # Doe John
+    instructors = [
+        Instructor("Doe", "John", {}),
+        Instructor("Smith", "Jane", {}),
+        Instructor("Brown", "Michael", {}),
+    ]
 
-    instructor.add_semester(1, ["Math", "Physics"])
-    instructor.add_semester(2, ["Chemistry"])
-    instructor.add_subject_to_semester("Biology", 1)
-    instructor.add_subjects(2)
-    instructor.remove_subject_from_semester("Math", 1)
-    instructor.print_semester_subjects(1)  # Number of subjects in semester 1: 2
-    # Subjects in semester 1:
-    # Physics
-    # Biology
+    for instructor in instructors:
+        print(instructor)  # Prints name and surname of the instructor
 
-    instructor.print_average_subjects()  # Average number of subjects per semester: 1.3333333333333333
-    instructor.print_semester_with_most_subjects()  # Semester with the most subjects: 1
-    # Number of subjects in semester 1: 2
+        # Add semesters and subjects to the instructor's schedule
+        instructor.add_semester(1, ["Math", "Physics"])
+        instructor.add_semester(2, ["Chemistry"])
+        instructor.add_subject_to_semester("Biology", 1)
+        instructor.add_subjects(2)
+        instructor.remove_subject_from_semester("Math", 1)
+        instructor.print_semester_subjects(1)  # Number of subjects in semester 1: 2
 
-    instructor.print_subjects_per_semester()  # Subjects per semester:
-    # Semester 1: 2 subjects
-    #     Physics
-    #     Biology
-    # Semester 2: 1 subjects
-    #     Chemistry
+        # Print statistics about the instructor's schedule
+        instructor.print_average_subjects()  # Average number of subjects per semester
+        instructor.print_semester_with_most_subjects()  # Semester with the most subjects
+        instructor.print_subjects_per_semester()  # List of subjects per semester
 
-    instructor.add_semester_with_subjects()  # Enter a semester number: 3
-    # Enter a subject for this semester (or 'done' to finish): History
-    # Enter a subject for this semester (or 'done' to finish): Geography
-    # Enter a subject for this semester (or 'done' to finish): done
-    # Added 2 subjects to semester 3
+        # Add a semester with user input
+        instructor.add_semester_with_subjects()
 
-    instructor.remove_semester(2)
-    instructor.print_subjects_per_semester()  # Subjects per semester:
-    # Semester 1: 2 subjects
-    #     Physics
-    #     Biology
-    # Semester 3: 2 subjects
-    #     History
-    #     Geography
+        # Remove a semester
+        instructor.remove_semester(2)
+
+        # Remove subjects from a semester with user input
+        semester_num = int(input("Enter the semester number to remove subjects from: "))
+        subjects_to_remove = []
+        while True:
+            subject = input("Enter a subject to remove from the semester (or 'done' to finish): ")
+            if subject == "done":
+                break
+            subjects_to_remove.append(subject)
+        instructor.remove_subject_from_semester(subjects_to_remove, semester_num)
+        instructor.print_semester_subjects(semester_num)
 
 
 if __name__ == "__main__":
