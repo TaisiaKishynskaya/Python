f63d174acd2d5f86feff8d6cf2330cb05e41b76d
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-04-01 12:33:05 +0300

    Add function in task 4

--- task_4.py
+++ task_4.py
@@ -1,31 +1,37 @@
-print('Introduction to programming: Task 2')
-print('Kyshynska Taisiia')
+def check_lucky_number(number):
+    sum_first = 0
+    sum_last = 0
 
-while True:
-    my_str = input('Input six-digit number or type "exit" to quit: ')
+    # We calculate the sum of the first and last three digits
+    for i in range(3):
+        sum_first += int(number[i])
+        sum_last += int(number[i + 3])
 
-    # we check that the user does not want to log out
-    if my_str.lower() == "exit":
-        break
-
-    # we check that the number is entered
-    if not my_str.isdigit():
-        print("Input a number!")
+    # We compare the sums of the first and last three digits
+    if sum_first == sum_last:
+        print("This is a lucky number!")
     else:
-        # we check that the number is six-digit
-        if len(my_str) != 6:
-            print("Input a six-digit number!")
+        print("This is not a lucky number.")
+
+
+if __name__ == '__main__':
+
+    print('Introduction to programming: Task 2')
+    print('Kyshynska Taisiia')
+
+    while True:
+        my_str = input('Input six-digit number or type "exit" to quit: ')
+
+        # we check that the user does not want to log out
+        if my_str.lower() == "exit":
+            break
+
+        # we check that the number is entered
+        if not my_str.isdigit():
+            print("Input a number!")
         else:
-            SUM_FIRST = 0
-            SUM_LAST = 0
-
-            # We calculate the sum of the first and last three digits
-            for i in range(3):
-                SUM_FIRST += int(my_str[i])
-                SUM_LAST += int(my_str[i + 3])
-
-            # We compare the sums of the first and last three digits
-            if SUM_FIRST == SUM_LAST:
-                print("This is a lucky number!")
+            # we check that the number is six-digit
+            if len(my_str) != 6:
+                print("Input a six-digit number!")
             else:
-                print("This is not a lucky number.")
+                check_lucky_number(my_str)
