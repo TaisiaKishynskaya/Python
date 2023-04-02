a7c1d940782e7181eb915adb80d67daca01d9804
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-04-02 21:32:20 +0300

    1. Add validations 2. Swapped 'if' and 'else' 3. Add 'update_semester()' instead of 'add_semester()' and 'remove_semester()' 4. Add '@property' 5. Use 'max(iterable, key=...)'

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from task_7 import Instructor
 
 def main():
     instructors = [
-        Instructor("Doe", "John", {}),
-        Instructor("Smith", "Jane", {}),
-        Instructor("Brown", "Michael", {}),
+        Instructor('Doe', 'John', {}),
+        Instructor('Smith', 'Jane', {}),
+        Instructor('Brown', 'Michael', {}),
     ]
 
     for instructor in instructors:
         print(instructor)  # Prints name and surname of the instructor
 
         # Add semesters and subjects to the instructor's schedule
-        instructor.add_semester(1, ["Math", "Physics"])
-        instructor.add_semester(2, ["Chemistry"])
-        instructor.add_subject_to_semester("Biology", 1)
+        instructor.update_semester(1, ['Math', 'Physics'])
+        instructor.update_semester(2, ['Chemistry'])
+        instructor.add_subject_to_semester('Biology', 1)
         instructor.add_subjects(2)
-        instructor.remove_subject_from_semester("Math", 1)
+        instructor.remove_subject_from_semester('Math', 1)
         instructor.print_semester_subjects(1)  # Number of subjects in semester 1: 2
 
         # Print statistics about the instructor's schedule
@@ -28,14 +28,14 @@ def main():
         instructor.add_semester_with_subjects()
 
         # Remove a semester
-        instructor.remove_semester(2)
+        instructor.update_semester(2)
 
         # Remove subjects from a semester with user input
-        semester_num = int(input("Enter the semester number to remove subjects from: "))
+        semester_num = int(input('Enter the semester number to remove subjects from: '))
         subjects_to_remove = []
         while True:
             subject = input("Enter a subject to remove from the semester (or 'done' to finish): ")
-            if subject == "done":
+            if subject == 'done':
                 break
             subjects_to_remove.append(subject)
         instructor.remove_subject_from_semester(subjects_to_remove, semester_num)
--- task_7.py
+++ task_7.py
@@ -1,81 +1,89 @@
+def validate_semester_number(semester):
+    if not isinstance(semester, int) or semester <= 0:
+        raise ValueError("Semester number must be a positive integer")
+
+
+def validate_semester_exists(semester, subjects_per_semester):
+    if semester not in subjects_per_semester:
+        raise ValueError(f"Semester {semester} does not exist")
+
+
 class Instructor:
     def __init__(self, last_name, first_name, subjects_per_semester):
-        self.last_name = last_name
-        self.first_name = first_name
-        self.subjects_per_semester = subjects_per_semester
+        self._last_name = last_name
+        self._first_name = first_name
+        self._subjects_per_semester = subjects_per_semester
 
     def __str__(self):
-        return f"{self.last_name} {self.first_name}"
+        return f'{self._last_name} {self._first_name}'
 
-    def add_subject_to_semester(self, subject, semester):
-        subjects = self.subjects_per_semester.get(semester, [])
-        subjects.append(subject)
-        self.subjects_per_semester[semester] = subjects
+    def add_subject_to_semester(self, subject, semester, subjects_per_semester):
+        validate_semester_number(semester)
+        validate_semester_exists(semester, subjects_per_semester)
+        self._subjects_per_semester.setdefault(semester, []).append(subject)
 
-    def remove_subject_from_semester(self, subject, semester):
-        subjects = self.subjects_per_semester.get(semester, [])
+    def remove_subject_from_semester(self, subject, semester, subjects_per_semester):
+        validate_semester_number(semester)
+        validate_semester_exists(semester, subjects_per_semester)
+        subjects = self._subjects_per_semester.get(semester, [])
         if subject in subjects:
-            self.subjects_per_semester[semester].remove(subject)
+            subjects.remove(subject)
         else:
-            raise ValueError(f"{subject} not found in semester {semester}")
+            raise ValueError(f'{subject} not found in semester {semester}')
+        self._subjects_per_semester[semester] = subjects
 
-    def add_semester(self, semester, subjects=None):
-        if subjects is None:
-            subjects = []
-        if semester in self.subjects_per_semester:
-            raise ValueError(f"Semester {semester} already exists")
-        self.subjects_per_semester[semester] = subjects
-
-    def remove_semester(self, semester):
-        if semester not in self.subjects_per_semester:
-            raise ValueError(f"Semester {semester} does not exist")
-        del self.subjects_per_semester[semester]
+    def update_semester(self, semester, subjects=None, subjects_per_semester=None):
+        validate_semester_number(semester)  # validation
+        if semester in self._subjects_per_semester and subjects is not None:
+            raise ValueError(f'Semester {semester} already exists')
+        validate_semester_exists(semester, subjects_per_semester)
+        self._subjects_per_semester[semester] = subjects
 
     def print_semester_subjects(self, semester):
-        subjects = self.subjects_per_semester.get(semester, [])
-        print(f"Number of subjects in semester {semester}: {len(subjects)}")
-        print(f"Subjects in semester {semester}:")
+        subjects = self._subjects_per_semester.get(semester, [])
+        print(f'Number of subjects in semester {semester}: {len(subjects)}')
+        print(f'Subjects in semester {semester}:')
         for subject in subjects:
             print(subject)
 
     def print_average_subjects(self):
-        num_semesters = len(self.subjects_per_semester)
-        num_subjects = sum(len(subjects) for subjects in self.subjects_per_semester.values())
-        print(f"Average number of subjects per semester: {num_subjects / num_semesters}")
+        num_semesters = len(self._subjects_per_semester)
+        num_subjects = sum(len(subjects) for subjects in self._subjects_per_semester.values())
+        print(f'Average number of subjects per semester: {num_subjects / num_semesters}')
+
+    @property
+    def semester_with_most_subjects(self):
+        max_semester = max(self._subjects_per_semester.keys(),
+                           key=lambda semester: len(self._subjects_per_semester[semester]))
+        return max_semester
 
     def print_semester_with_most_subjects(self):
-        max_subjects = -1
-        max_semester = None
-        for semester, subjects in self.subjects_per_semester.items():
-            num_subjects = len(subjects)
-            if num_subjects > max_subjects:
-                max_subjects = num_subjects
-                max_semester = semester
-        print(f"Semester with the most subjects: {max_semester}")
-        print(f"Number of subjects in semester {max_semester}: {max_subjects}")
+        max_semester = self.semester_with_most_subjects
+        print(f'Semester with the most subjects: {max_semester}')
+        print(f'Number of subjects in semester {max_semester}: {len(self._subjects_per_semester[max_semester])}')
 
     def print_subjects_per_semester(self):
-        print("Subjects per semester:")
-        for semester, subjects in self.subjects_per_semester.items():
-            print(f"Semester {semester}: {len(subjects)} subjects")
+        print('Subjects per semester:')
+        for semester, subjects in self._subjects_per_semester.items():
+            print(f'Semester {semester}: {len(subjects)} subjects')
             for subject in subjects:
-                print(f"\t{subject}")
+                print(f'\t{subject}')
 
-    def add_subjects(self, semester):
-        if semester not in self.subjects_per_semester:
-            raise ValueError(f"Semester {semester} does not exist")
-        subjects = self.subjects_per_semester.get(semester, [])
+    def _get_subjects(self, semester, subjects_per_semester):
+        validate_semester_exists(semester, subjects_per_semester)
+        return self._subjects_per_semester.setdefault(semester, [])
+
+    def add_subjects(self, semester, subjects_per_semester):
+        subjects = self._get_subjects(semester, subjects_per_semester)
         while True:
             subject = input("Enter a subject to add or 'done' to finish: ")
             if subject.lower() == 'done':
                 break
             subjects.append(subject)
-        self.subjects_per_semester[semester] = subjects
+        self._subjects_per_semester[semester] = subjects
 
-    def remove_subjects(self, semester):
-        if semester not in self.subjects_per_semester:
-            raise ValueError(f"Semester {semester} does not exist")
-        subjects = self.subjects_per_semester.get(semester, [])
+    def remove_subjects(self, semester, subjects_per_semester):
+        subjects = self._get_subjects(semester, subjects_per_semester)
         while True:
             subject = input("Enter a subject to remove or 'done' to finish:")
             if subject.lower() == 'done':
@@ -83,24 +91,24 @@ class Instructor:
             if subject in subjects:
                 subjects.remove(subject)
             else:
-                print(f"{subject} is not found in semester {semester}")
-        self.subjects_per_semester[semester] = subjects
-        print(f"Removed {len(subjects)} subjects from semester {semester}")
+                print(f'{subject} is not found in semester {semester}')
+        self._subjects_per_semester[semester] = subjects
+        print(f'Removed {len(subjects)} subjects from semester {semester}')
 
     def add_semester_with_subjects(self):
-        semester = input("Enter a semester number: ")
+        semester = input('Enter a semester number: ')
         try:
             semester = int(semester)
         except ValueError:
-            print("Invalid semester number. Please enter an integer.")
+            print('Invalid semester number. Please enter an integer.')
             return
-        if semester in self.subjects_per_semester:
-            print(f"Semester {semester} already exists. Adding subjects to existing semester.")
+        if semester not in self._subjects_per_semester:
+            self._subjects_per_semester[semester] = []
         else:
-            self.subjects_per_semester[semester] = []
+            print(f'Semester {semester} already exists. Adding subjects to existing semester.')
         while True:
             subject = input("Enter a subject for this semester (or 'done' to finish): ")
             if subject == 'done':
                 break
-            self.subjects_per_semester[semester].append(subject)
-        print(f"Added {len(self.subjects_per_semester[semester])} subjects to semester {semester}")
+            self._subjects_per_semester[semester].append(subject)
+        print(f'Added {len(self._subjects_per_semester[semester])} subjects to semester {semester}')
