da5ffd112ce6a4c29470f39ad2b6ad4a06766151
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-04-03 21:21:29 +0300

    Fix linter (in test for task 5.1)

--- tests/unit/test_task_5_1.py
+++ tests/unit/test_task_5_1.py
@@ -13,29 +13,3 @@ from task_5_1 import get_sum
 )
 def test_get_sum(epsilon, my_row, expected_sum):
     assert abs(get_sum(epsilon, my_row) - expected_sum) < epsilon
-
-
-"""def test_get_sum():
-    # Test 1: Check if the function returns the expected output for a given input
-    epsilon = 0.001
-    my_row = [1, 0.5, 0.25, 0.125, 0.0625, 0.03125]
-    expected_sum = 1.96875
-    assert abs(get_sum(epsilon, my_row) - expected_sum) < epsilon
-
-    # Test 2: Check if the function returns the expected output for another input
-    epsilon = 0.00001
-    my_row = [1, 1 / 4, 1 / 9, 1 / 16, 1 / 25, 1 / 36]
-    expected_sum = 1.4913888888888889
-    assert abs(get_sum(epsilon, my_row) - expected_sum) < epsilon
-
-    # Test 3: Check if the function returns the expected output for an empty row
-    epsilon = 0.001
-    my_row = []
-    expected_sum = 0
-    assert get_sum(epsilon, my_row) == expected_sum
-
-    # Test 4: Check if the function returns the expected output for a single element row
-    epsilon = 0.001
-    my_row = [1]
-    expected_sum = 1
-    assert get_sum(epsilon, my_row) == expected_sum"""
