22a11739e903586f2523977b26e70510ec95c3df
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-04-03 20:44:53 +0300

    Done test for task 5.1

--- task_5_1.py
+++ task_5_1.py
@@ -4,10 +4,15 @@ from utils import get_name_string, get_intro_string
 get_name_string()
 get_intro_string()
 
+
+def get_sum(epsilon, my_row):
+    # the sum of the series with the specified accuracy:
+    suma_row = sum(filter(lambda x: x != 0, takewhile(lambda x: x >= epsilon, my_row)))
+    print(f'The sum of the series with eps={epsilon} accuracy is: {suma_row}')
+    return suma_row
+
+
 if __name__ == '__main__':
     EPSILON = 0.0001  # accuracy
     row = (1 / pow(2, n) + 1 / pow(3, n) for n in count())  # series generator
-    # the sum of the series with the specified accuracy:
-    suma = sum(filter(lambda x: x != 0, takewhile(lambda x: x >= EPSILON, row)))
-
-    print(f'The sum of the series with eps={EPSILON} accuracy is: {suma}')
+    suma = get_sum(EPSILON, row)
--- tests/unit/test_task_5.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-
-def test_something():
-    assert True  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
