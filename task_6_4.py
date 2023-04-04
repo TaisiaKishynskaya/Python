bc5a6e0e836cd7b13fa3b46c2e792a27d240b34f
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-04-05 00:02:42 +0300

    Redone task 6.4 (without tests)

--- task_6_4.py
+++ task_6_4.py
@@ -1,12 +1,31 @@
-FILE_NAME = 'learning_python.txt'
+def open_file(file_name):
+    try:
+        with open(file_name, encoding='utf-8') as it_file:
+            return it_file
+    except IOError:
+        print(f'Error: could not open file {file_name} for reading.')
+        return None
 
-try:
-    with open(FILE_NAME, encoding='utf-8') as file:
-        lines = file.readlines()
-except IOError:
-    print(f'Ошибка: не удалось открыть файл {FILE_NAME} для чтения.')
-else:
-    print('Содержимое файла:')
-    for line in lines:
+
+def read_file(the_file):
+    if the_file is None:
+        return []
+    the_lines = the_file.readlines()
+    return the_lines
+
+
+def replace_and_print(our_lines):
+    if our_lines is None:
+        return []
+    print('File contents:')
+    for line in our_lines:
         new_line = line.replace('Python', 'C')
         print(new_line.strip())
+    return our_lines
+
+
+if __name__ == '__main__':
+    FILE_NAME = 'learning_python.txt'
+    file = open_file(FILE_NAME)
+    lines = read_file(file)
+    replace_and_print(lines)
