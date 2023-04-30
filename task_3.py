fe17f20ca5b34ccc76ae33616e5972f68ad2d829
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-04-30 22:12:04 +0300

    Try to fix task 3

--- task_3.py
+++ task_3.py
@@ -3,9 +3,6 @@ from utils import print_intro, get_input_value
 
 print_intro()
 
-MY_X = 'x'
-MY_Z = 'z'
-
 
 def calculate_y(x_val, z_val):
     if (x_val ** 2) * abs(x_val - z_val) == 1:
@@ -14,19 +11,19 @@ def calculate_y(x_val, z_val):
         print((x_val + m.exp(z_val - 1)) / (1 - (x_val ** 2) * abs(x_val - z_val)))
 
 
-x = get_input_value(MY_X)
-z = get_input_value(MY_Z)
+x = get_input_value('x')
+z = get_input_value('z')
 
 while True:
     calculate_y(x, z)
     choice = input('Do you want to change the values of x or z? (y/n) ')
     if choice.lower() == 'y':
-        var_choice = input(f'Which variable do you want to change? ({MY_X}/{MY_Z}) ')
+        var_choice = input('Which variable do you want to change? (x/z) ')
 
-        if var_choice.lower() == MY_X:
-            x = get_input_value(MY_X)
-        elif var_choice.lower() == MY_Z:
-            z = get_input_value(MY_Z)
+        if var_choice.lower() == 'x':
+            x = get_input_value('x')
+        elif var_choice.lower() == 'z':
+            z = get_input_value('z')
         else:
             print('Invalid input')
 
