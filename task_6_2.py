d16b4652d1ea70f0a2d1cb702402a7cfb3a20940
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-12 18:10:43 +0300

    Add functions in task 6.2

--- task_6_2.py
+++ task_6_2.py
@@ -1,7 +1,7 @@
 import os
 
 
-def is_valid_number(num_str):
+def validate_number(num_str):
     """Функция для проверки, является ли строка числом.
     Возвращает True, если строка является числом, и False в противном случае."""
     try:
@@ -11,46 +11,73 @@ def is_valid_number(num_str):
         return False
 
 
-def is_odd(the_num):
-    """Функция для проверки, является ли число нечетным.
-    Возвращает True, если число нечетное, и False в противном случае."""
-    return the_num % 2 == 1
-
-
 def write_to_file(the_filename, the_text):
     """Функция для записи текста в файл с заданным именем."""
     with open(the_filename, 'w', encoding='utf-8') as file:
         file.write(the_text)
 
 
-def main():
+def check_even_odd(num):
+    """Функция для определения четности числа и возврата текстовой строки."""
+    if num == 0:
+        result = 'neither even nor odd'
+    elif num % 2 == 1:
+        result = 'odd'
+    else:
+        result = 'even'
+    return f'Число {num} - {result}.'
+
+
+def get_integer_input():
+    """Функция для получения от пользователя целого числа."""
     while True:
-        # Получаем число от пользователя и проверяем его
         input_str = input('Введите целое число: ')
-        if not is_valid_number(input_str):
+        if not validate_number(input_str):
             print('Ошибка: введенное значение не является целым числом.')
             continue
+        return int(input_str)
+
+
+def write_result_to_file(text):
+    """Функция для записи результата в файл."""
+    filename = 'result.txt'
+    if os.path.exists(filename):
+        choice = input(f'Файл {filename} уже существует, перезаписать? (y/n): ')
+        check_yes_or_no(choice)
+    write_to_file(filename, text)
+    print(f'Результат записан в файл {filename}.')
+
+
+def ask_to_continue():
+    """Функция для запроса у пользователя продолжения выполнения программы."""
+    choice = input('Хотите продолжить выполнение программы? (y/n): ').lower()
+    check_yes_or_no(choice)
+
+
+def check_yes_or_no(choice):
+    while True:
+        if choice.lower() == 'n':
+            return False
+        elif choice.lower() == 'y':
+            return True
+        else:
+            choice = input('Ошибка: неверный ввод. Введите "y" или "n": ')
+            continue
+
+
+def main():
+    while True:
+        input_str = get_integer_input()
 
         # Преобразуем введенную строку в число
         num = int(input_str)
 
-        # Определяем, является ли число нечетным, и выводим соответствующее сообщение на экран
-        result = 'нечетное' if is_odd(num) else 'четное'
-        print(f'Число {num} - {result}.')
-        text = f'Число {num} - {result}.'
+        text = check_even_odd(num)
+        print(text)
 
-        # Создаем файл и записываем в него текст
-        filename = 'result.txt'
-        if os.path.exists(filename):
-            choice = input(f'Файл {filename} уже существует, перезаписать? (y/n): ')
-            if choice.lower() == 'n':
-                break
-        write_to_file(filename, text)
-        print(f'Результат записан в файл {filename}.')
+        write_result_to_file(text)
 
-        # Проверяем, хочет ли пользователь продолжить выполнение программы
-        choice = input('Хотите продолжить выполнение программы? (y/n): ')
-        if choice.lower() == 'n':
+        if not ask_to_continue():
             break
 
 
