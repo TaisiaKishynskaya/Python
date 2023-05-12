b2c111ccd470c66f7784a4f1bdfcd4a2bdd56dfc
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-12 23:23:35 +0300

    Add functions in task 6.3

--- task_6_3.py
+++ task_6_3.py
@@ -1,9 +1,12 @@
-def write_to_file(my_filename, my_lines):
+FILENAME = 'learning_python.txt'
+
+
+def write_to_file(file_name, my_lines):
     try:
-        with open(my_filename, 'w', encoding='utf-8') as file:  # Здесь код для записи в файл
+        with open(file_name, 'w', encoding='utf-8') as file:  # Здесь код для записи в файл
             file.writelines(my_lines)
     except IOError:
-        print(f'Ошибка: не удалось открыть файл {my_filename} для записи.')
+        print(f'Error: could not open file {file_name} for writing.')
 
 
 def read_file(the_filename):
@@ -11,22 +14,27 @@ def read_file(the_filename):
         with open(the_filename, encoding='utf-8') as file:
             the_lines = file.readlines()
     except IOError:
-        print(f'Ошибка: не удалось открыть файл {the_filename} для чтения.')
+        print(f'Error: could not open file {the_filename} for reading.')
         return None
-
     return the_lines
 
 
-if __name__ == '__main__':
-    FILENAME = 'learning_python.txt'
-
+def create_learning_python_file(filename):
     lines = ['In Python you can easily manipulate strings.\n',
              'In Python you can use a variety of data structures.\n',
              'In Python you can write modular and reusable code.\n']
-    write_to_file(FILENAME, lines)
+    write_to_file(filename, lines)
+    return filename
 
+
+def print_learning_python_file():
     lines = read_file(FILENAME)
     if lines:
-        print('Содержимое файла:')
+        print('File`s content:')
         for line in lines:
             print(line.strip())
+
+
+if __name__ == '__main__':
+    create_learning_python_file(FILENAME)
+    print_learning_python_file()
