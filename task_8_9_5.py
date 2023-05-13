23c1382ad7d946dcd518e2b59501d52656072ca5
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-13 19:06:18 +0300

    Done task 8-9.5 (without tests)

---
+++ task_8_9_5.py
@@ -0,0 +1,26 @@
+month_days = {'january': 31,
+              'february': '28 or 29',
+              'march': 31,
+              'april': 30,
+              'may': 31,
+              'june': 30,
+              'july': 31,
+              'august': 31,
+              'september': 30,
+              'october': 31,
+              'november': 30,
+              'december': 31}
+
+
+def print_days_in_month(month, days_in_month):
+    # Check if the correct month name has been entered
+    if days_in_month is None:
+        print('Invalid month name.')
+    else:
+        print(f'Number of days in {month}: {days_in_month}')
+
+
+if __name__ == '__main__':
+    month_name = input("Enter month name: ")
+    days = month_days.get(month_name.lower())  # Get the number of days in a month
+    print_days_in_month(month_name, days)
