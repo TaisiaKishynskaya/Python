92facaeb3ab6e5dcf06efc360ea3aae28ffea24a
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-13 18:09:52 +0300

    Add functions, simplified task 6.7

--- task_6_7.py
+++ task_6_7.py
@@ -1,5 +1,8 @@
-with open('book.txt', 'r', encoding='utf-8') as file:
-    text = file.read().replace('\n', ' ')
+def main():
+    with open('book.txt', 'r', encoding='utf-8') as file, open('formatted_text.txt', 'w', encoding='utf-8') as out_file:
+        text = file.read().replace('\n', ' ')
+        out_file.write(text)
 
-with open('formatted_text.txt', 'w', encoding='utf-8') as file:
-    file.write(text)
+
+if __name__ == '__main__':
+    main()
