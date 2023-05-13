60f35e14163f4aa793db5287fb65b724625a29b4
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-13 18:10:13 +0300

    Add function in task 6.8

--- task_6_8.py
+++ task_6_8.py
@@ -1,13 +1,19 @@
 import re
 
-# Opening files for reading and writing
-with open('The Life and Adventures of Robinson Crusoe.txt', 'r', encoding='utf-8') as file, \
-        open('chapters.txt', 'w', encoding='utf-8') as out_file:
-    text = file.read()
 
-    # Search for section headings using a regular expression
-    chapter_titles = re.findall(r'CHAPTER [IVXLC]+—[A-Z\s’\-–—]+\.', text)
+def main():
+    # Opening files for reading and writing
+    with open('The Life and Adventures of Robinson Crusoe.txt', 'r', encoding='utf-8') as file, \
+            open('chapters.txt', 'w', encoding='utf-8') as out_file:
+        text = file.read()
 
-    # Write the found headers to a file
-    for title in chapter_titles:
-        out_file.write(title + '\n')
+        # Search for section headings using a regular expression
+        chapter_titles = re.findall(r'CHAPTER [IVXLC]+—[A-Z\s’\-–—]+\.', text)
+
+        # Write the found headers to a file
+        for title in chapter_titles:
+            out_file.write(title + '\n')
+
+
+if __name__ == '__main__':
+    main()
