404fc8d2b3f62f89a41d7bc6218e97f0fe4562ea
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-13 19:35:16 +0300

    Done task 8-9.6 (without tests)

---
+++ task_8_9_6.py
@@ -0,0 +1,8 @@
+def main():
+    year = int(input("Enter year: "))
+    print("Leap year." if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+          else "Ordinary year.")
+
+
+if __name__ == '__main__':
+    main()
