9037ab09db5b98f4daca6e593a466e713483e587
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-13 20:45:21 +0300

    Add utils in task 8-9.6

--- task_8_9_6.py
+++ task_8_9_6.py
@@ -1,3 +1,8 @@
+from utils import print_intro
+
+print_intro()
+
+
 def main():
     year = int(input("Enter year: "))
     print("Leap year." if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
