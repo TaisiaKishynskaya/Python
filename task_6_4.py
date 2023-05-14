c36504a5af9b7eef2cfd18adb4d121f977512611
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-14 22:12:46 +0300

    Shortened the program

--- task_6_4.py
+++ task_6_4.py
@@ -1,33 +1,10 @@
-FILE_NAME = 'learning_python.txt'
-
-
-def open_file(file_name):
-    try:
-        with open(file_name, encoding='utf-8') as it_file:
-            return it_file
-    except IOError:
-        print(f'Error: could not open file {file_name} for reading.')
-        return None
-
-
-def read_file(the_file):
-    if the_file is None:
-        return []
-    the_lines = the_file.readlines()
-    return the_lines
-
-
-def replace_and_print(our_lines):
-    if our_lines is None:
-        return []
-    print('File contents:')
-    for line in our_lines:
+def replace_and_print():
+    with open('learning_python.txt', encoding='utf-8') as file:
+        lines = file.readlines()
+    for line in lines:
         new_line = line.replace('Python', 'C')
         print(new_line.strip())
-    return our_lines
 
 
 if __name__ == '__main__':
-    file = open_file(FILE_NAME)
-    lines = read_file(file)
-    replace_and_print(lines)
+    replace_and_print()
