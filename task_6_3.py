7f8941a3619633929977ae87ee1c4901560ca316
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-14 21:56:11 +0300

    Shortened the program

--- task_6_3.py
+++ task_6_3.py
@@ -1,26 +1,6 @@
-FILENAME = 'learning_python.txt'
-
-
-def write_to_file(file_name, my_lines):
-    try:
-        with open(file_name, 'w', encoding='utf-8') as file:
-            file.writelines(my_lines)
-    except IOError:
-        print(f'Error: could not open file {file_name} for writing.')
-
-
-def read_file(the_filename):
-    try:
-        with open(the_filename, encoding='utf-8') as file:
-            the_lines = file.readlines()
-    except IOError:
-        print(f'Error: could not open file {the_filename} for reading.')
-        return None
-    return the_lines
-
-
 def print_learning_python_file():
-    lines = read_file(FILENAME)
+    with open('learning_python.txt', encoding='utf-8') as file:
+        lines = file.readlines()
     if lines:
         print('File`s content:')
         for line in lines:
