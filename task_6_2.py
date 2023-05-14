33449e0daad0d562f10025c1182fa5625fa80b6b
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-14 21:38:14 +0300

    Shortened the program

--- task_6_2.py
+++ task_6_2.py
@@ -1,22 +1,3 @@
-import os
-
-
-def validate_number(num_str):
-    """Функция для проверки, является ли строка числом.
-    Возвращает True, если строка является числом, и False в противном случае."""
-    try:
-        int(num_str)
-        return True
-    except ValueError:
-        return False
-
-
-def write_to_file(the_filename, the_text):
-    """Функция для записи текста в файл с заданным именем."""
-    with open(the_filename, 'w', encoding='utf-8') as file:
-        file.write(the_text)
-
-
 def check_even_odd(num):
     """Функция для определения четности числа и возврата текстовой строки."""
     if num == 0:
@@ -28,57 +9,14 @@ def check_even_odd(num):
     return f'Число {num} - {result}.'
 
 
-def get_integer_input():
-    """Функция для получения от пользователя целого числа."""
-    while True:
-        input_str = input('Введите целое число: ')
-        if not validate_number(input_str):
-            print('Ошибка: введенное значение не является целым числом.')
-            continue
-        return int(input_str)
-
-
 def write_result_to_file(text):
     """Функция для записи результата в файл."""
     filename = 'result.txt'
-    if os.path.exists(filename):
-        if not get_yes_no_input(f'Файл {filename} уже существует, перезаписать? (y/n): '):
-            return
-    write_to_file(filename, text)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(check_even_odd(text))
     print(f'Результат записан в файл {filename}.')
 
 
-def ask_to_continue():
-    """Функция для запроса у пользователя продолжения выполнения программы."""
-    return get_yes_no_input('Хотите продолжить работу программы? (y/n): ')
-
-
-def get_yes_no_input(prompt):
-    """Функция для получения от пользователя ответа y/n на заданный вопрос."""
-    while True:
-        choice = input(prompt).lower()
-        if choice == 'n':
-            return False
-        if choice == 'y':
-            return True
-        print('Ошибка: неверный ввод. Введите "y" или "n".')
-
-
-def main():
-    while True:
-        input_str = get_integer_input()
-
-        # Преобразуем введенную строку в число
-        num = int(input_str)
-
-        text = check_even_odd(num)
-        print(text)
-
-        write_result_to_file(text)
-
-        if not ask_to_continue():
-            break
-
-
 if __name__ == '__main__':
-    main()
+    number = int(input('Введите целое число: '))
+    write_result_to_file(number)
--- task_6_3.py
+++ task_6_3.py
@@ -3,7 +3,7 @@ FILENAME = 'learning_python.txt'
 
 def write_to_file(file_name, my_lines):
     try:
-        with open(file_name, 'w', encoding='utf-8') as file:  # Здесь код для записи в файл
+        with open(file_name, 'w', encoding='utf-8') as file:
             file.writelines(my_lines)
     except IOError:
         print(f'Error: could not open file {file_name} for writing.')
@@ -19,14 +19,6 @@ def read_file(the_filename):
     return the_lines
 
 
-def create_learning_python_file(filename):
-    lines = ['In Python you can easily manipulate strings.\n',
-             'In Python you can use a variety of data structures.\n',
-             'In Python you can write modular and reusable code.\n']
-    write_to_file(filename, lines)
-    return filename
-
-
 def print_learning_python_file():
     lines = read_file(FILENAME)
     if lines:
@@ -36,5 +28,4 @@ def print_learning_python_file():
 
 
 if __name__ == '__main__':
-    create_learning_python_file(FILENAME)
     print_learning_python_file()
