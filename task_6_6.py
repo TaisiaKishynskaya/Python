55ed1f2109643cdb94edef0fe323c433251cea12
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-14 22:55:28 +0300

    Shortened the program

--- task_6_6.py
+++ task_6_6.py
@@ -1,34 +1,18 @@
-import sys
-
 FILE_NAME = 'gutenberg.txt'
 
 
 def read_file(file_name):
-    try:
-        with open(file_name, 'r', encoding='utf-8') as file:
-            content = file.read()
-    except FileNotFoundError:
-        print(f"Error: File '{file_name}' not found.")
-        sys.exit()
+    with open(file_name, 'r', encoding='utf-8') as file:
+        content = file.read()
     return content
 
 
-def read_and_split_file(file_name):
-    try:
-        return read_file(file_name).split('\n')
-    except FileNotFoundError:
-        print(f"Error: File '{file_name}' not found.")
-        sys.exit()
-
-
-def count_occurrences(arr_texts):
-    for i, text in enumerate(arr_texts):
-        if not text:
-            continue
+def count_occurrences(text):
+    for i, text in enumerate(text):
         count = text.lower().count('the')
         print(f'Text {i + 1}: {count}')
 
 
 if __name__ == '__main__':
-    texts = read_and_split_file(FILE_NAME)
-    count_occurrences(texts)  # We count the number of occurrences of the word "the" in each text
+    texts = read_file(FILE_NAME).split('\n')
+    count_occurrences(texts)
