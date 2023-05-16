9a821a606837381b1bc45fe023e8111c7d194056
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-16 17:50:04 +0300

    Clear program

--- task_8_9_12.py
+++ task_8_9_12.py
@@ -4,40 +4,40 @@ from utils import print_intro
 print_intro()
 
 
-def get_computer_choice():
-    choices = ['rock', 'paper', 'scissors']
-    return random.choice(choices)
+ROCK = 'rock'
+PAPER = 'paper'
+SCISSORS = 'scissors'
+CHOISES = [ROCK, PAPER, SCISSORS]
+TIE = 'tie'
 
 
 def get_user_choice():
     while True:
         choice = input('Choose rock, paper, or scissors: ')
-        if choice in ['rock', 'paper', 'scissors']:
+        if choice in CHOISES:
             return choice
         print('Invalid input. Please choose rock, paper, or scissors.')
 
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        return 'tie'
-    if user_choice == 'rock' and computer_choice == 'scissors':
-        return 'user'
-    if user_choice == 'paper' and computer_choice == 'rock':
-        return 'user'
-    if user_choice == 'scissors' and computer_choice == 'paper':
+        return TIE
+    if user_choice == ROCK and computer_choice == SCISSORS or \
+            user_choice == PAPER and computer_choice == ROCK or \
+            user_choice == SCISSORS and computer_choice == PAPER:
         return 'user'
     return 'computer'
 
 
 def play_game():
     user_choice = get_user_choice()
-    computer_choice = get_computer_choice()
+    computer_choice = random.choice(CHOISES)
 
     print(f'You chose {user_choice}. The computer chose {computer_choice}.')
 
     winner = determine_winner(user_choice, computer_choice)
-    if winner == 'tie':
-        print("It's a tie!")
+    if winner == TIE:
+        print(f"It's a {TIE}!")
     else:
         print(f'The {winner} wins!')
 
