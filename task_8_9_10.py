2730b0cfd5a85f14dfcd708c0f3fd4d36078c22f
TaisiaKishynskaya <tayusha201449@gmail.com>
2023-05-16 16:56:10 +0300

    Add validations

--- task_8_9_10.py
+++ task_8_9_10.py
@@ -6,28 +6,17 @@ print_intro()
 def get_position():
     while True:
         position = input('Enter the position of the chess piece (e.g. a1): ')
-        if len(position) == 1:
-            print('Invalid input. Please enter both a letter between a-h and a number between 1-8.')
-            continue
-
-        column = position[0]  # letter
-        row = position[1]  # number
-
-        # Check if the values are within the allowed limits
-        if column not in 'abcdefgh' or row not in '12345678':
+        if len(position) != 2 or position[0] not in 'abcdefgh' or position[1] not in '12345678':
             print('Invalid input. Please enter a letter between a-h and a number between 1-8.')
         else:
-            return column, row
+            return position[0], int(position[1])
 
 
 def determine_color(let, num):
-    if (ord(let) + int(num)) % 2 == 0:
-        return 'black'
-    return 'white'
+    return 'black' if (ord(let) + num) % 2 == 0 else 'white'
 
 
 if __name__ == '__main__':
-    # pylint: disable=invalid-name
     letter, number = get_position()
     color = determine_color(letter, number)
-    print(f'The square at {letter+number} is {color}.')
+    print(f'The square at {letter+str(number)} is {color}.')
--- task_8_9_11.py
+++ task_8_9_11.py
@@ -3,25 +3,45 @@ from utils import print_intro
 print_intro()
 
 
+def validate_number_input(input_str):
+    try:
+        int(input_str)
+    except ValueError:
+        print('Invalid input. Please enter a valid number.')
+        return False
+    return True
+
+
+def get_valid_number_input(message):
+    number = input(message)
+    while not validate_number_input(number):
+        number = input(message)
+    return int(number)
+
+
 def convert_decimal_to_binary():
-    decimal_num = int(input('Enter a decimal number: '))
+    decimal_num = get_valid_number_input('Enter a decimal number: ')
+
     binary_num = ''
     while decimal_num > 0:
         remainder = decimal_num % 2
         binary_num = str(remainder) + binary_num
         decimal_num = decimal_num // 2
+
     print(f'{decimal_num} in Decimal is {binary_num} in Binary')
 
 
 def convert_binary_to_decimal():
     binary_num = input('Enter a binary number: ')
-    decimal_num = 0
-    power = 0  # to keep track of the current power of two
+    while not all(char in '01' for char in binary_num):
+        binary_num = input('Invalid input. Please enter a valid binary number: ')
 
-    # loop through the digits in reverse order,coz start with the values of the smaller digits
+    decimal_num = 0
+    power = 0
+
     for digit in binary_num[::-1]:
         decimal_num += int(digit) * 2 ** power
-        power += 1  # increases with each new digit in the binary number
+        power += 1
 
     print(f'{binary_num} in Binary is {decimal_num} in Decimal')
 
